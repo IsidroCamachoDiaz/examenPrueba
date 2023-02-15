90a92fd7ba57cecf94ce66c09c6dd132f8443c29
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-02-15 10:02:27 +0100

    15-2-2023

---
+++ examenPrueba/src/main/webapp/script/script.js
@@ -0,0 +1,19 @@
+
+function dni(numero){
+	//Si no cumple slae la alerta
+	if(numero<99999999||numero>999999999){
+		alert("Este dni no es valido")
+	}
+	//Si cumple entra
+	else{
+		var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+		//el parseInt loc convierte en entero
+		let numeroLetra=0;
+		numeroLetra=numero%23;
+		let letra='C';
+		letra=letras[numeroLetra];
+		console.log(numero)
+		console.log(letra)
+		document.write(numero+"-"+letra)
+	}
+}
